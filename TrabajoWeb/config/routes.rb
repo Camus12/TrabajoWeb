90049c434cf44c065860ecd2c0992c55d57f4215
Bryant <bryant.tello12@gmail.com>
2019-01-22 16:29:05 -0500

    Segundo commit

--- TrabajoWeb/config/routes.rb
+++ TrabajoWeb/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/vista'
+  get 'home/login'
+  get 'home/registro'
+  get 'home/bajausuario'
+  get 'home/actualizaciondeusuario'
+  get 'home/index' 
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
