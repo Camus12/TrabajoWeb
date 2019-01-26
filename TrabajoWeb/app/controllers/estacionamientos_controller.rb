82826f1e748fb90b6ecaeae8782bcff5b72e44f6
Daniel Davila <u201618048@upc.edu.pe>
2019-01-26 13:39:27 -0500

    Cambios previos a la expo 1 en clientes

--- TrabajoWeb/app/controllers/estacionamientos_controller.rb
+++ TrabajoWeb/app/controllers/estacionamientos_controller.rb
@@ -1,13 +1,18 @@
 class EstacionamientosController < ApplicationController
-  layout 'dueno'
-	
+  layout 'dueno', except: [:buscar, :buscar_detalle]
+	#layout 'cliente', except: [:edicion, :lista]
+
   def edicion
   end
 
   def lista
   end
 
-
   def buscar
+    render layout: "cliente"
+  end
+
+  def buscar_detalle
+    render layout: "cliente"
   end
 end
