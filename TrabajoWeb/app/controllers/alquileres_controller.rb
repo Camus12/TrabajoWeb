8141c2deaba04f6018e203c2275b455c0d6ea28d
Dixson Vasquez <dixson.vasquez@gmail.com>
2019-02-12 00:06:14 -0500

    Dixson - registro de alquiler

--- TrabajoWeb/app/controllers/alquileres_controller.rb
+++ TrabajoWeb/app/controllers/alquileres_controller.rb
@@ -28,18 +28,46 @@ class AlquileresController < ApplicationController
 
 
   def registro
-  #logger.debug params
- #   @estacionamiento = Estacionamiento.find(params[:id])
-  #end
+    
+    @estacionamiento = Estacionamiento.find(params[:id])
+    #logger.debug @estacionamiento  
 
-   @estacionamiento = Estacionamiento.new(estacionamiento_params)
+   #@estacionamiento = Estacionamiento.new(estacionamiento_params)
 
-    if @estacionamiento.save
-      redirect_to :action => :show, :id => @estacionamiento.id
-    else
-      render "new"    
-    end
+    #if @estacionamiento.save
+      #redirect_to :action => :show, :id => @estacionamiento.id
+    #else
+    #  render "new"    
+    #end
   end
+
+  def save_alquiler
+    logger.debug params
+     @alquiler = Alquiler.new(alquiler_params)
+     
+    logger.debug @alquiler     
+    @alquiler.inicioalquiler =  DateTime.strptime(params[:upc][:fecha] + " " + params[:upc][:hora],"%d/%m/%Y %H:%M")
+    #calcula fechafin , solo se suma una hora
+    horas = 1
+    @alquiler.finalquiler = (@alquiler.inicioalquiler + 60*60*horas )
+     #calcula monto total
+    @alquiler.preciototal = @alquiler.precioporhora * horas 
+    
+      if @alquiler.save
+        redirect_to '/estacionamientos/buscar'
+        #format.html { {:controller => :estacionamientos, :action => :buscar} notice: 'Registro exitoso' }
+
+      else
+        format.html { render :registro }
+
+      end
+    
+
+  end
+  def alquiler_params
+      params.require(:upc).permit( :codigopersona, :codigoestacionamiento, :precioporhora)
+
+    end
 
 end
 
